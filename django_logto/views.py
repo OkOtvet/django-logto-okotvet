9c43bec18c7b5b50b06f68b4d052925f1b7ed085
Gerzhan <nikolay.gerzhan@gmail.com>
2025-01-01 20:26:25 +0700

    perf: use all constants from django settings

--- django_logto/views.py
+++ django_logto/views.py
@@ -1,14 +1,13 @@
-import os
-
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views import View
+from django.conf import settings
 
 from .logto_init import start_logto_client
 
 
 class SigninView(View):
     async def get(self, request):
-        uri = os.environ.get("LOGTO_API_REDIRECT_URI")
+        uri = settings.LOGTO_API_REDIRECT_URI
         client = start_logto_client(request)
         url = await client.signIn(redirectUri=uri)
         return HttpResponseRedirect(redirect_to=url)
@@ -21,7 +20,7 @@ class CallbackView(View):
         try:
             await client.handleSignInCallback(absolute_uri)
             return HttpResponseRedirect(
-                redirect_to=os.environ.get("LOGTO_CALLBACK_URI")
+                redirect_to=settings.LOGTO_CALLBACK_URI
             )
         except Exception as e:
             return HttpResponse("Error: " + str(e))
